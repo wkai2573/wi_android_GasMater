1c2a58cbf15d5702e65ee9ef74ceac3a11312430
kai <kai.wei@wavein.com.tw>
2023-10-23 11:38:11 +0800

    feat: csv改成與舊版匹配,未使用欄位保留原值

--- app/src/main/java/com/wavein/gasmeter/data/model/MeterRow.kt
+++ app/src/main/java/com/wavein/gasmeter/data/model/MeterRow.kt
@@ -10,22 +10,46 @@ import com.wavein.gasmeter.tools.toBoolean10
 import com.wavein.gasmeter.tools.toString10
 
 data class MeterRow(
-	val group:String,                            // 群組
-	val queue:Int,                               // 抄表順序
+	// 原csv欄位(不照順序,*號為csv欄位名)
+	val group:String,                            // 群組 *群組號
+	val queue:Int,                               // 抄表順序 *抄錶順路
 	val custId:String,                           // 客戶編號
 	val custName:String,                         // 姓名
 	val custAddr:String,                         // 地址
+	val remark:String? = null,                   // 記事
 
-	val meterId:String,                          // 瓦斯表ID
-	val meterDegree:Float? = null,               // 抄表值
-	val meterReadTime:String? = null,            // 抄表時間
-	val meterState:String? = null,               // 表狀態 (メーター状態 16-02)
-	val alarmInfo1:String? = null,               // 告警設定 (アラーム情報1 03-01)
+	val meterId:String,                          // 瓦斯表ID *通信ID
+	val meterDegree:Float? = null,               // 抄表值 *抄錶數值
+	val meterReadTime:String? = null,            // 抄表時間 *抄錶日期
+	val alarmInfo1:String? = null,               // 告警1 (アラーム情報1 03-01)
+	val alarmInfo2:String? = null,               // 告警2 (アラーム情報2 03-02)
 
 	val batteryVoltageDropAlarm:Boolean? = null, // 電池電壓警報
 	val innerPipeLeakageAlarm:Boolean? = null,   // 洩漏警報
 	val shutoff:Boolean? = null,                 // 遮斷
+	val shutoffTime:String? = null,              // 遮斷時間 *遮斷日期
+
+	val lastMeterDegree:Float? = null,           // 前次抄表值 *前次數值
+	val lastMeterReadTime:String? = null,        // 前次抄表時間 *前次日期
+	val manualMeterDegree:Float? = null,         // 人工輸入 *人工输入
+
+	// 原csv欄位,未使用
+	val callingChannel:String? = null,           // 通信CH
+	val businessesCode:String? = null,           // 事業体碼
+	val dash:String? = null,                     // -
+	val relayId:String? = null,                  // 中繼ID
+	val areaName:String? = null,                 // 區域名稱
+	val areaRelayNo:String? = null,              // 區域中繼No
+	val areaRelayName:String? = null,            // 區域中繼名稱
+
+	// 新csv欄位
+	// todo 壓力數值
+
+	// 其他欄位
+	val meterState:String? = null,               // 表狀態  (メーター状態 16-02)
 ) {
+	// 使用量
+	val degreesUsed get() = if (meterDegree == null || lastMeterDegree == null) null else meterDegree - lastMeterDegree
 
 	// 已抄數量提示
 	val degreeRead:Boolean get() = meterDegree != null // 已抄表
@@ -60,18 +84,32 @@ fun Map<String, String>.toMeterRow():MeterRow? {
 	try {
 		val csvRow = this
 		return MeterRow(
-			group = csvRow["群組"] ?: return null,
-			queue = csvRow["抄表順序"]?.toInt() ?: return null,
+			queue = csvRow["抄錶順路"]?.toInt() ?: return null,
 			custId = csvRow["客戶編號"] ?: return null,
 			custName = csvRow["姓名"] ?: return null,
+			dash = csvRow["-"] ?: return null,
 			custAddr = csvRow["地址"] ?: return null,
-			meterId = csvRow["瓦斯表ID"] ?: return null,
-			meterDegree = csvRow["抄表值"]?.toFloatOrNull(),
-			alarmInfo1 = csvRow["告警設定"],
-			meterReadTime = csvRow["抄表時間"],
+			remark = csvRow["記事"] ?: return null,
+			meterId = csvRow["通信ID"] ?: return null,
+			relayId = csvRow["中繼ID"] ?: return null,
+			group = csvRow["群組號"] ?: return null,
+			areaName = csvRow["區域名稱"] ?: return null,
+			areaRelayNo = csvRow["區域中繼No"] ?: return null,
+			areaRelayName = csvRow["區域中繼名稱"] ?: return null,
+			meterDegree = csvRow["抄錶數值"]?.toFloatOrNull(),
+			manualMeterDegree = csvRow["人工输入"]?.toFloatOrNull(),
+			meterReadTime = csvRow["抄錶日期"] ?: return null,
 			batteryVoltageDropAlarm = csvRow["電池電壓警報"].toBoolean10(),
 			innerPipeLeakageAlarm = csvRow["洩漏警報"].toBoolean10(),
 			shutoff = csvRow["遮斷"].toBoolean10(),
+			shutoffTime = csvRow["遮斷日期"] ?: return null,
+			callingChannel = csvRow["通信CH"] ?: return null,
+			businessesCode = csvRow["事業体碼"] ?: return null,
+			lastMeterDegree = csvRow["前次數值"]?.toFloatOrNull(),
+			lastMeterReadTime = csvRow["前次日期"] ?: return null,
+			//degreesUsed = csvRow["使用量"]?.toFloatOrNull(),
+			alarmInfo1 = csvRow["告警1"] ?: return null,
+			alarmInfo2 = csvRow["告警2"] ?: return null,
 		)
 	} catch (e:Exception) {
 		e.printStackTrace()
@@ -87,18 +125,32 @@ fun List<Map<String, String>>.toMeterRows():List<MeterRow> {
 // meterRow 轉 csvRow
 private fun MeterRow.toCsvRow():Map<String, String> {
 	return mapOf(
-		"群組" to this.group,
-		"抄表順序" to this.queue.toString(),
+		"抄錶順路" to this.queue.toString(),
 		"客戶編號" to this.custId,
 		"姓名" to this.custName,
+		"-" to this.dash.orEmpty(),
 		"地址" to this.custAddr,
-		"瓦斯表ID" to this.meterId,
-		"抄表值" to (this.meterDegree?.toString() ?: ""),
-		"告警設定" to this.alarmInfo1.orEmpty(),
-		"抄表時間" to this.meterReadTime.orEmpty(),
+		"記事" to this.remark.orEmpty(),
+		"通信ID" to this.meterId,
+		"中繼ID" to this.relayId.orEmpty(),
+		"群組號" to this.group,
+		"區域名稱" to this.areaName.orEmpty(),
+		"區域中繼No" to this.areaRelayNo.orEmpty(),
+		"區域中繼名稱" to this.areaRelayName.orEmpty(),
+		"抄錶數值" to (this.meterDegree?.toString() ?: ""),
+		"人工输入" to (this.manualMeterDegree?.toString() ?: ""),
+		"抄錶日期" to this.meterReadTime.orEmpty(),
 		"電池電壓警報" to this.batteryVoltageDropAlarm.toString10(),
 		"洩漏警報" to this.innerPipeLeakageAlarm.toString10(),
 		"遮斷" to this.shutoff.toString10(),
+		"遮斷日期" to this.shutoffTime.orEmpty(),
+		"通信CH" to this.callingChannel.orEmpty(),
+		"事業体碼" to this.businessesCode.orEmpty(),
+		"前次數值" to (this.lastMeterDegree?.toString() ?: ""),
+		"前次日期" to this.lastMeterReadTime.orEmpty(),
+		"使用量" to (this.degreesUsed?.toString() ?: ""),
+		"告警1" to this.alarmInfo1.orEmpty(),
+		"告警2" to this.alarmInfo2.orEmpty(),
 	)
 }
 
